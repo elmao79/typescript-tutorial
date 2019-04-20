9bfb1668552893eb8c1b491b33408f7df9b5aca8
OSCAR MARTIN <elmao79@gmail.com>
2019-04-20 21:05:50 +0200

    Typescript configuration udpated

--- src/ts7.ts
+++ src/ts7.ts
@@ -1,10 +1,10 @@
-type Lengthable<T> = { [P in keyof T]: T[P] extends Array<any> ? T[P] : never };
+type Lengthable<T> = { [P in keyof T]: T[P] extends any[] ? T[P] : never };
 
-type Company = {
+interface Company {
     employees: string[]; // Lengthable
     supervisors: string[]; // Lengthable
     name: string;
-};
+}
 
 const logCompany = ({ name, employees, supervisors }: { name: string; employees: Lengthable<any>; supervisors: Lengthable<any> }): void =>
     console.log(`${name} Employees: ${employees.length} Supervisors: ${supervisors.length}`);
@@ -12,5 +12,5 @@ const logCompany = ({ name, employees, supervisors }: { name: string; employees:
 const company = { name: 'My Company', employees: ['CARMEN', 'MARCO'], supervisors: ['OSCAR', 'MARICARMEN'] };
 logCompany(company);
 
-const company2 = { name: 'My Company 2', employees: 'no employees', supervisors: 'no supervisors' };
+// const company2 = { name: 'My Company 2', employees: 'no employees', supervisors: 'no supervisors' };
 // logCompany(company2); Error
